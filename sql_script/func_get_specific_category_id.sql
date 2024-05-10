1fdaeb7da5cdb30eefde2bc2a17e9dff6cd49d55
Arajdian Altaf <arajdianaltaf123@gmail.com>
2024-05-10 16:01:23 +0700

    Compare sub_category_id to get unique result

--- sql_script/func_get_specific_category_id.sql
+++ sql_script/func_get_specific_category_id.sql
@@ -1,13 +1,12 @@
 delimiter //
-create function get_specific_category_id(spec_cat_name varchar(255)) RETURNS INT deterministic
+create function get_specific_category_id(sub_cat_id INT, spec_cat_name varchar(255)) RETURNS INT deterministic
 begin
 	declare spec_cat_id INT;
     
     select specific_cat_id
     into spec_cat_id
-    from specific_category
-    where specific_cat_name = spec_cat_name
-    collate utf8mb4_general_ci;
+    from specific_category spc
+    where spc.specific_cat_name = spec_cat_name AND spc.sub_category_id = sub_cat_id;
     
     if spec_cat_id is null then
 		set spec_cat_id = 0;
